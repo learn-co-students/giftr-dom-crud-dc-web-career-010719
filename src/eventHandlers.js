7264da6a440e2e43d11593b4636e20e72f1d2dd9
canikwe <c.anikwe@gmail.com>
2019-05-20 10:59:13 -0400

    completed lab

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -1,1 +1,73 @@
-// You can add your event handlers here! 
+function filterGifts() {
+  const filter = document.querySelector('#filter-input')
+
+  filter.addEventListener('keydown', (e) => {
+    document.querySelector('.gift-list').childNodes.forEach(g => {
+      if (!g.innerText.toLowerCase().includes(e.target.value.toLowerCase())) {
+        g.style = 'display:none'
+      } else {
+        g.style = ''
+      }
+    })
+  })
+}
+
+function handleGiftForm() {
+  const form = document.querySelector('#gift-form')
+  const giftList = document.querySelector('.gift-list')
+  
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    
+    const li = document.createElement('li')
+    const img = document.createElement('img')
+    const name = form.name.value
+    const image = form.image.value
+    const delBtn = document.createElement('button')
+    const editBtn = document.createElement('button')
+
+    if (name === '' || image === '') {
+      alert('please fill out the form')
+    } else {
+      delBtn.innerText = 'DELETE'
+      editBtn.innerText = 'EDIT'
+      img.src = image
+      li.innerHTML = `<p>${name}</p> <br><br>`
+  
+      li.append(img, delBtn, editBtn)
+      giftList.appendChild(li)
+
+      delBtn.addEventListener('click', () => giftList.removeChild(li))
+      editBtn.addEventListener('click', () => editGift(li))
+  
+      form.reset()
+    }
+  })
+
+}
+
+function editGift(li) {
+  console.log('edit this thing!', name)
+
+  const inputName = document.createElement('input')
+  const inputImg = document.createElement('input')
+  const p = document.createElement('p')
+  const br = document.createElement('br')
+  const submitBtn = document.createElement('button')
+
+  inputName.value = li.querySelector('p').innerText
+  inputImg.value = li.querySelector('img').src
+  submitBtn.innerText = 'Submit'
+
+  li.append(p, inputName, br , inputImg, submitBtn)
+
+  submitBtn.addEventListener('click', () => {
+    console.log(li.querySelector('p'))
+    li.querySelector('p').innerText = inputName.value
+    li.querySelector('img').src = inputImg.value
+
+    li.removeChild(inputName)
+    li.removeChild(inputImg)
+    li.removeChild(submitBtn)
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM has been fully loaded')
   console.table(gifts)
+  init()
 })
+
+
+function init(){
+  displayGifts()
+  filterGifts()
+  handleGiftForm()
+}
+
+function displayGifts() {
+  let giftContainer = document.querySelector(".gift-list")
+  giftContainer.innerHTML = ""
+  
+  gifts.forEach(gift => {
+    let li = document.createElement("li")
+    let gImg = document.createElement("img")
+    let delBtn = document.createElement('button')
+    let editBtn = document.createElement('button')
+    
+    delBtn.innerText = 'DELETE'
+    editBtn.innerText = 'EDIT'
+    gImg.src = gift.image
+    giftContainer.appendChild(li)
+    li.innerHTML = `<p>${gift.name}</p> <br></br>`
+    li.append(gImg, delBtn, editBtn)
+
+    delBtn.addEventListener('click', () => giftContainer.removeChild(li))
+    editBtn.addEventListener('click', () => editGift(li))
+  })
+}
